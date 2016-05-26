22d26c23815c884dcc596cb3dd4292feb53cf724
Mark Stewart <markstewart@dbc-markstewart.local>
2016-05-26 13:59:46 -0400

    fix Timeable bug

--- lib/dbc_today/timeable.rb
+++ lib/dbc_today/timeable.rb
@@ -4,8 +4,7 @@ module Timeable
   def in_military(time_string)
     return '00:00' unless time_string =~ /\d(:\d{2})?(a|p)m/
 
-    time_string = time_string.dup
-    normalized_time = time_string.insert(-3, ':00') unless time_string.include?(':')
+    normalized_time = normalize_time(time_string.dup)
 
     meridian = normalized_time.slice!(/(a|p)m/)
     if meridian == 'am' || normalized_time.include?('12:')
@@ -22,7 +21,10 @@ module Timeable
   end
 
   def normalize_time(time_string)
-    if 
-    
+    if time_string.include?(':')
+      time_string
+    else
+      time_string.insert(-3, ':00')
+    end
   end
 end
