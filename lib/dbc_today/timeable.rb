71b3fdbfe4e3f9dfb9bf566f9b47d8ddf48da627
Mark Stewart <markstewart@dbc-markstewart.local>
2016-05-26 13:57:11 -0400

    rearrange Timeable

--- lib/dbc_today/timeable.rb
+++ lib/dbc_today/timeable.rb
@@ -3,13 +3,15 @@ module Timeable
 
   def in_military(time_string)
     return '00:00' unless time_string =~ /\d(:\d{2})?(a|p)m/
+
     time_string = time_string.dup
-    time_string = time_string.insert(-3, ':00') unless time_string.include?(':')
-    meridian = time_string.slice!(/(a|p)m/)
-    if meridian == 'am' || time_string.include?('12:')
-      time_string.rjust(5, '0')
+    normalized_time = time_string.insert(-3, ':00') unless time_string.include?(':')
+
+    meridian = normalized_time.slice!(/(a|p)m/)
+    if meridian == 'am' || normalized_time.include?('12:')
+      normalized_time.rjust(5, '0')
     else
-      add_twelve_hours(time_string)
+      add_twelve_hours(normalized_time)
     end
   end
 
@@ -17,5 +19,10 @@ module Timeable
     hours, minutes = time_string.split(':')
     new_hours = (hours.to_i + 12).to_s
     "#{new_hours}:#{minutes}"
+  end
+
+  def normalize_time(time_string)
+    if 
+    
   end
 end
